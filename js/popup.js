0e7976c812f5f7e2a95300e69b3f44ae378ddf5a
Viktoriia Nosova <viktoriianosova@hotmail.com>
2017-12-12 13:01:02 +0800

    Критерий Д18
    Интерактивная карта(не забыть про close-button)

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,52 @@
 var linkForm = document.querySelector(".popup-button");
-var form = document.querySelector(".modal-form");
-var close = document.querySelector(".button-close")
+var popupForm = document.querySelector(".modal-form");
+var close = document.querySelector(".button-close");
+var userName = popupForm.querySelector("[name=user-name]");
+var userEmail = popupForm.querySelector("[name=user-email]");
+// var storage = localStorage.getItem("userName", "userEmail");
+
+var linkMap = document.querySelector(".popup-map");
+var popupMap = document.querySelector(".modal-map");
+
+// Modal Form
 
 linkForm.addEventListener("click", function(evt) {
   evt.preventDefault();
-  form.classList.add("modal-show");
+  popupForm.classList.add("modal-show");
+  userName.focus();
 });
 
 close.addEventListener("click", function(evt) {
   evt.preventDefault();
-  form.classList.remove("modal-show");
+  popupForm.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (popupForm.classList.contains("modal-show")) {
+      popupForm.classList.remove("modal-show");
+    }
+  }
+});
+
+// Form animation
+
+// Modal Map
+
+linkMap.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupMap.classList.add("modal-show");
+});
+
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupMap.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (popupMap.classList.contains("modal-show")) {
+      popupMap.classList.remove("modal-show");
+    }
+  }
 });
